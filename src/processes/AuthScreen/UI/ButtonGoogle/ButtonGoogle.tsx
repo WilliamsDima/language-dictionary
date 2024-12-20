bbe32e6e5522049e319087d8a4306f03fe67362b
dmitry <oxpa97@mail.ru>
2024-12-20 19:29:43 +0300

    alert add

--- src/processes/AuthScreen/UI/ButtonGoogle/ButtonGoogle.tsx
+++ src/processes/AuthScreen/UI/ButtonGoogle/ButtonGoogle.tsx
@@ -3,7 +3,7 @@ import { styles } from './ButtonGoogle.styles'
 import Button from '@/shared/UI/Button/Button'
 import GoogleIcon from '@/assets/icons/UI/google.svg'
 import Text from '@/shared/UI/Text/Text'
-import { View } from 'react-native'
+import { Alert, View } from 'react-native'
 import auth from '@react-native-firebase/auth'
 import { GoogleSignin } from '@react-native-google-signin/google-signin'
 import { useActions } from '@/shared/hooks/useActions'
@@ -14,17 +14,23 @@ const ButtonGoogle: FC<Props> = (props) => {
   const { setIsAuth } = useActions()
   // Handle user state changes
   const onAuthStateChanged = (user: any) => {
+    Alert.alert('onAuthStateChanged user', JSON.stringify(user))
+    console.log('onAuthStateChanged user', user)
+
     setIsAuth(!!user)
   }
 
   const onGoogleButtonPress = async () => {
+    Alert.alert('onGoogleButtonPress')
     try {
       // Check if your device supports Google Play
       await GoogleSignin.hasPlayServices()
       // Get the users ID token
       const userInfo = await GoogleSignin.signIn()
 
-      // console.log('userInfo', userInfo)
+      console.log('userInfo', userInfo)
+
+      Alert.alert('userInfo', JSON.stringify(userInfo))
 
       // Create a Google credential with the token
       const googleCredential = auth.GoogleAuthProvider.credential(
@@ -36,6 +42,7 @@ const ButtonGoogle: FC<Props> = (props) => {
       // Sign-in the user with the credential
       return auth().signInWithCredential(googleCredential)
     } catch (error) {
+      Alert.alert('onGoogleButtonPress error', JSON.stringify(error))
       console.log('onGoogleButtonPress error', error)
     }
   }
